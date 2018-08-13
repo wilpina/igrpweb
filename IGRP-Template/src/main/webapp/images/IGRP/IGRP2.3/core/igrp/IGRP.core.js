04fe4385013afde9b43e43eb43c5d3a6930095c9
Isaias1994GitHub <joaquim199410@gmail.com>
2018-08-13 15:35:26 -0100

    update 13-08-2018

--- IGRP-Template/src/main/webapp/images/IGRP/IGRP2.3/core/igrp/IGRP.core.js
+++ IGRP-Template/src/main/webapp/images/IGRP/IGRP2.3/core/igrp/IGRP.core.js
@@ -5,6 +5,17 @@
 
 			url : {
 
+				isUrl : function(url){
+					var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+					  	'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+					  	'((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+					  	'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+					  	'(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+					  	'(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+
+					return pattern.test(url);
+				},
+
 				getParams : function (url) {
 
 				 	var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
@@ -301,8 +312,9 @@
 				if(p.name){
 					if ($('input[name="'+p.name+'"]')[0]) 
 						$('input[name="'+p.name+'"]').addClass(clss).val(p.value);
-					else
+					else if(id)
 						form.prepend('<input id="'+id+'" type="hidden" class="'+clss+'" name="'+p.name+'" value="'+p.value+'"/>');
+					else form.prepend('<input type="hidden" class="'+clss+'" name="'+p.name+'" value="'+p.value+'"/>');
 				}
 			},
 			htmlEncode:function(str){
