bf1da171deb6bc88cf4344e16e637d67ce577c9c
Isaias1994GitHub <joaquim199410@gmail.com>
2018-11-30 09:51:08 -0100

    update 30-11-2018

--- IGRP-Template/src/main/webapp/images/IGRP/IGRP2.3/core/fileeditor/igrp.fileeditor.js
+++ IGRP-Template/src/main/webapp/images/IGRP/IGRP2.3/core/fileeditor/igrp.fileeditor.js
@@ -11,7 +11,11 @@
 
 	function FileEditor(dom){
 
-		var fileEditor = this;
+		var fileEditor = this,
+
+			newRequest = null,
+
+			createURL  = $(dom).attr('create-url');
 
 		fileEditor.templates = app.classes.get('templates');
 
@@ -19,6 +23,7 @@
 
 		fileEditor.viewr   	 = $('.igrp-fileeditor-tab',dom);
 
+		fileEditor.addModal = $('#fileeditor-add-modal');
 
 		function removeEditorsErrors(resize){
 		
@@ -33,7 +38,8 @@
 			if(resize)
 				
 				resizeCodeMirrorArea();
-		}
+
+		};
 
 		function resizeCodeMirrorArea(){
 
@@ -53,7 +59,7 @@
 				
 			});
 
-		}
+		};
 
 		function showEditorsErrors(jsonRes,editor){
 
@@ -95,7 +101,7 @@
 				}
 			}
 											
-		}
+		};
 
 		function Save(){
 
@@ -171,6 +177,7 @@
 			}
 
 			return false;
+
 		};
 
 		function GetActiveTab(){
@@ -183,65 +190,294 @@
 
 			return active.pane.find('.CodeMirror');
 
-		}
+		};
 
-		function Events(){
+		function OpenFile(e){
 
-			$('li.file',fileEditor.menu).on('click',function(){
+			var li 		= this,
 
-				var li 		= this,
+				fileId  = $(li).attr('file-id'),
 
-					fileId  = $(li).attr('file-id'),
+				tabID   = 'file-'+$(li).attr('id'),
 
-					tabID   = 'file-'+$(li).attr('id'),
+				path 	= $(li).attr('file-path'),
 
-					path 	= $(li).attr('file-path'),
+				name    = $(li).attr('title'),
 
-					name    = $(li).attr('title'),
+				file    = $(li).attr('file-name');
 
-					file    = $(li).attr('file-name');
+			if(!li.request){
 
-				if(!li.request){
+				$(li).addClass('loading');
 
-					$(li).addClass('loading');
+				li.request = $.get( path );
 
-					li.request = $.get( path );
+				li.request.then( function(d){
 
-					li.request.then( function(d){
+					fileEditor.viewr.Tab('add-item',{
 
-						fileEditor.viewr.Tab('add-item',{
+						id      : tabID,
 
-							id      : tabID,
+						title   : name,
 
-							title   : name,
+						ident   : fileId,
 
-							ident   : fileId,
+						content : GetCodeEditor( { id : tabID, content : d } ),
 
-							content : GetCodeEditor( { id : tabID, content : d } ),
+						active  : true,
 
-							active  : true,
+						close   : true,
 
-							attrs   : {
+						attrs   : {
 
-								'file-name' : file
+							'file-name' : file
 
-							}
+						},
 
-						});
+						callback:function(r){
 
-						$(li).removeClass('loading');
+							var closer = $('<div class="fileeditor-menu-closer"><i class="fa fa-times"></i></div>');
+
+							closer.on('click',function(){
+
+								r.body.remove();
+
+								r.li.remove();
+
+								li.request = null;
+
+								$(li).removeClass('active');
+
+								$('.igrp-fileeditor-tab ul li a').first().click();
+
+							});
+
+							r.li.append(closer);
+
+						}
 
 					});
 
-				}else{
+					ResizeTab();
 
-					fileEditor.viewr.Tab('activate', tabID );
+					$(li).removeClass('loading');
 
-				}
+				});
+
+			}else{
+
+				fileEditor.viewr.Tab('activate', tabID );
+
+			}
+
+		};
+
+		function AddNew(e){
+		
+			if(!newRequest){
+
+				var input = $(this),
+
+					text  = input.val(),
+
+					options = input.data('add-options'),
+
+					enter = e.keyCode === 13 ? true : false,
+
+					blur  = e.type == 'focusout',
+
+					esc   = e.keyCode === 27,
+
+					remove = function(){
+
+						try{
+
+			        		input.parents('.'+options.type).first().remove();
+
+			        	}catch(er){
+			        		
+			        		null;
+
+			        	}
+
+					};
+
+				if ( (enter || blur )  && !esc ){
+			        
+			        e.preventDefault();
+
+			        if(text){
+
+			        	var options = input.data('add-options');
+
+			        	if(options){
+
+			        		$('#igrp-fileeditor').addClass('requesting');
+
+			        		options.item.addClass('loading');
+
+			        		newRequest = $.post( createURL, {
+
+					        	type : options.type,
+
+					        	path : options.path,
+
+					        	file_type : options.fileType,
+
+					        	name : text
+
+					        }).then(function(r){
+
+					        	var newItem = options.type == 'folder' ? $(fileEditor.templates.treeItem(r)) : $(fileEditor.templates.folderFiles([r])) 
+
+					        	options.item.replaceWith( newItem );
+
+					        	newRequest = null;
+
+					        	newItem.click();
+
+					        }).always(function(){
+
+					        	options.item.remove();
+
+					        	$('#igrp-fileeditor').removeClass('requesting');
+
+					        });
+
+			        	}
+
+			        }else
+
+			        	remove();
+
+			        e.stopPropagation();
+
+			        return false;
+
+			    }
+
+			    if (e.keyCode === 27 ) 
+
+			    	remove();
+
+			    if(options.fileExtension && e.keyCode != 8){
+			   		
+			   		var ext     = options.fileExtension,
+
+			   			startAt = input[0].selectionStart,
+
+			   			newVal = replaceSpecialChars( text.split('.'+ext)[0] );
+
+			   		input.val( newVal+'.'+ext );
+
+			   		input[0].selectionStart= startAt;
+
+			   	}
+
+			   	if(options.type == 'folder')
+
+			   		input.val( replaceSpecialChars( text.toLowerCase() ) );
+			};
+
+		    return false;
+
+		};
+
+		function DrawNew(o){
+
+			var container = o.folder.find('>.collapse>ul'),
+
+				item      = $(fileEditor.templates.newItem(o.type)),
+
+				operation = o.type == 'folder' ? 'prepend' : 'append';
+
+			o.item = item;
+
+			$('input',item).data('add-options',o);
+
+			container[operation]( item );
+
+			o.folder.find('>span>.dropdown>a').dropdown("toggle");
+
+			o.folder.find('>.collapse').collapse("show");			
+
+			$('.adder-input',container).focus();
+
+		};
+
+		function ShowAddMenu(e){
+
+			e.preventDefault();
+
+			$(this).find('>span>.dropdown>a.dropdown-toggle').click()
+
+			return false;
+		
+		};
+
+		function HandleNew(e){
+
+			e.preventDefault();
+
+			var folder   = $(this).parents('li.folder').first(),
+
+				type     = $(this).attr('type'),
+
+				fileType =  $(this).attr('file-type'),
+
+				fileExt  =  $(this).attr('file-ext'),
+
+				dirPath  = folder.attr('dir-path');
+
+			DrawNew({
+
+				folder : folder,
+
+				type : type,
+
+				path : dirPath,
+
+				fileType : fileType,
+
+				fileExtension : fileExt
 
 			});
 
-			$('.'+selectors.saveClss,dom).on('click', Save);
+			return false;
+
+		};
+
+		function ResizeTab(){
+
+			/*var ul   = $('ul',fileEditor.viewr),
+
+				items = $('>li',ul),
+
+				itemsWidth = 0;
+
+			items.each(function(i,item){
+				itemsWidth+= $(item).width() + 4
+			});
+
+			ul.width( itemsWidth )
+
+			console.log(itemsWidth)*/
+		
+		};
+
+		function Events(){
+
+			$(fileEditor.menu).on('click','li.file:not(.brand-new)', OpenFile );
+
+			$(dom).on('click','.add-new', HandleNew );
+
+			$(dom).on('contextmenu','li.folder', ShowAddMenu );
+
+			$(dom).on('keyup blur','.adder-input', AddNew );
+
+			//$(dom).on('keydown','.adder-input', ValidateNewItemName );
+
+			$(dom).on('click','.'+selectors.saveClss, Save);
 
 		};
 
@@ -250,6 +486,8 @@
 			fileEditor.menu = $(fileEditor.templates.tree(dir));
 
 			$('.'+selectors.leftPanelClss,dom).html(fileEditor.menu);
+
+			ResizeTab();
 
 			Events();
 
@@ -303,13 +541,44 @@
 
 			});
 
-
 		};
 
 		function ErrorHandler(e){
 
 			console.log(e);
 			
+		};
+
+		function replaceSpecialChars(label){
+			var chars = ['[',']','€','«','»',';','='];
+			var accents = [
+				{ base:'a', accents:['\u00c4' ,'\u00c3','\u00e1','\u00e0','\u00e2','\u00e3','\u00e4','\u00c1','\u00c0','\u00c2'] },
+				{ base:'e', accents:['\u00e9','\u00e8','\u00ea','\u00ea','\u00c9','\u00c8','\u00ca','\u00cb'] },
+				{ base:'i', accents:['\u00ed','\u00ec','\u00ee','\u00ef','\u00cd','\u00cc','\u00ce','\u00cf'] },
+				{ base:'o', accents:['\u00d6','\u00d5','\u00f3','\u00f2','\u00f4','\u00f5','\u00f6','\u00d3','\u00d2','\u00d4'] },
+				{ base:'u', accents:['\u00fa','\u00f9','\u00fb','\u00fc','\u00da','\u00d9','\u00db'] },
+				{ base:'c', accents:['\u00e7','\u00c7'] },
+				{ base:'n', accents:['\u00f1','\u00d1'] }
+			];
+
+			//replace white spaces to _
+			var str = label.replaceAll(' ','_');
+			//replace - to _
+			str = str.replaceAll('-','_');
+			//replace special characters
+			str = str.replace(/[&\/\\#,+()$~%.'":*?<>!?@´ªº^|£§{}]/g,'');
+			//replace symbols
+			chars.forEach(function(c){
+				str = str.replaceAll(c,'');
+			});
+			//replace accents
+			accents.forEach(function(aObj){
+				aObj.accents.forEach(function(a){
+					str = str.replaceAll(a,aObj.base);
+				});
+			});
+
+			return str;
 		};
 
 		Config();
