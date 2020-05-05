0740a60975cd9848db7aa45781fe2cc05a5ff248
Isaias.Nunes <Isaias.Nunes@NOSI-DDS-44.gov.cv>
2020-05-05 16:40:44 -0100

    v200505

--- IGRP-Template/src/main/webapp/images/IGRP/IGRP2.3/plugins/sharpadbclient/sharpadbclient.js
+++ IGRP-Template/src/main/webapp/images/IGRP/IGRP2.3/plugins/sharpadbclient/sharpadbclient.js
@@ -4,22 +4,25 @@
 
     $.IGRP.component('sharpadbclient', {
 
-        call: function (p) {
+        call : function (p) {
             //$('.sa-nsweb').attr('src', "SACWebAPI:" + JSON.stringify(p));
+
             var notify = p.notify ? p.notify : true,
-                dad    = $('#p_dad')[0] && $('#p_dad').val() ? 'prod' : 'all';
+                port   = $('#p_dad')[0] && $('#p_dad').val() ? $('#p_dad').val() : 'all';
+
+            port = $.IGRP.components.sharpadbclient.serviceport[port];
 
             $.ajax({
-                url     : com.urlservice[dad] + p.task ,
-                method  : 'POST',
-                data    : JSON.stringify(p.data),
-                contentType: "application/json",
-                dataType: "json",
-                success : function(s){
-                    if(s){
-                        if(p.success)
+                url         : port + '/api/' + p.task,
+                method      : 'POST',
+                data        : JSON.stringify(p.data),
+                contentType : "application/json",
+                dataType    : "json",
+                success     : function (s) {
+                    if (s) {
+                        if (p.success)
                             p.success(s);
-                        
+
                         if (notify) {
                             $.IGRP.notify({
                                 message: s.message,
@@ -28,23 +31,25 @@
                         }
                     }
                 },
-                error : function(e){
-                    
-                    if(p.error)
+                error    : function (e) {
+
+                    if (p.error)
                         p.error(e);
-                    
-                    if(notify){
-                        if (e.hasOwnProperty('responseJSON')){
+
+                    if (notify) {
+
+                        if (e.hasOwnProperty('responseJSON')) {
+
                             var resp = e.responseJSON;
 
-                            if (resp && resp.hasOwnProperty('errors')){
+                            if (resp && resp.hasOwnProperty('errors')) {
 
                                 var errors = resp.errors;
 
-                                for( n in errors){
-                                    
+                                for (n in errors) {
+
                                     errors[n].forEach(function (er) {
-            
+
                                         $.IGRP.notify({
                                             message: er,
                                             type   : 'danger'
@@ -59,69 +64,72 @@
             })
         },
 
-        urlservice : {
-            prod: 'https://sac-hostservice.nosi.cv:10110/SACWebAPI/',
-            all : 'https://sac-hostservice.nosi.cv:44326/SACWebAPI/'
+        serviceport : {
+            prod    : 'https://sac-hostservice.nosi.cv:10110',
+            dev     : 'https://sac-hostservice.nosi.cv:44326',
+            all 	: 'http://localhost:5000'
         },
 
-        actions : function(str){
+        actions: function (str) {
 
             var jsonEvents = {
-                start : {
-                    task: 'evaluationtask',
-                    id  : 1
+                start    : {
+                    task : 'evaluationtask',
+                    id   : 1
                 },
-                stop : {
-                    task: 'evaluationtask',
-                    id  : 2
+                stop     : {
+                    task : 'evaluationtask',
+                    id   : 2
                 },
-                sync : {
-                    task: 'evaluationtask',
-                    id  : 3
+                sync     : {
+                    task : 'evaluationtask',
+                    id   : 3
                 },
-                pause : {
-                    task: 'evaluationtask',
-                    id  : 4
+                pause    : {
+                    task : 'evaluationtask',
+                    id   : 4
                 },
-                config : {
-                    task: 'apptask',
-                    id  : 1
+                config   : {
+                    task : 'apptask',
+                    id   : 1
                 },
-                install : {
-                    task: 'apptask',
-                    id  : 2
+                install  : {
+                    task : 'apptask',
+                    id   : 2
                 },
-                uninstall : {
-                    task: 'apptask',
-                    id  : 3
+                uninstall: {
+                    task : 'apptask',
+                    id   : 3
                 }
             }
 
             return jsonEvents[str];
         },
 
-        run : function(p){
-            var action = com.actions(p.clicked.attr('sharpadbclient')),
+        run : function (p) {
+            
+            var action = com.actions(p.clicked.attr('sharpadbclient')), 
+
                 params = {
-                    data : {
-                        id  : action.id
+                    data   : {
+                        id : action.id
                     },
-                    task : action.task
+                    task   : action.task
                 };
 
-            if(p.url)
+            if (p.url)
                 params.data['url'] = p.url;
 
-            if(p.success)
+            if (p.success)
                 params['success'] = p.success;
 
-            if(p.error)
+            if (p.error)
                 params['error'] = p.error;
 
-            com.call(params);
+            com.call(params,com);
         },
 
-        init: function () {
+        init : function () {
             com = this;
         }
     }, true);
